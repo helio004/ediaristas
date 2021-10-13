11a11843d6158d99b62f75170c7be45c9d1f596a
Helio <heliosouza@consolidesuamarca.com.br>
2021-10-13 10:40:53 -0300

    feat: start next.js project

---
+++ src/ui/components/data-display/PageTitle/PageTitle.style.tsx
@@ -0,0 +1,13 @@
+import { styled } from "@mui/material/styles";
+
+export const PageTitleContainer = styled('div')`
+    margin: 40px 0;
+    text-align: center;
+`;
+
+
+export const PageTitleStyled = styled('h2')`
+    margin: 0;
+    color: #6B2AEE;
+    font-weight: 600;
+`
